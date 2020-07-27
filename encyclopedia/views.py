86ea225ddfe24fee4c1c65765ef8a2a8eccb0140
Daniel Johnston <danielj36@gmail.com>
2020-07-26 20:35:21 -0500

    slight tinkering

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
 
 
@@ -8,3 +8,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, name):
+    return render(request, "encyclopedia/test.html", {
+        "name": name
+    })
